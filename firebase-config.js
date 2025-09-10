7b12eb62e14238f1c65b57681e18d922936970aa
Ryo Fujimura <ryo176999@gmail.com>
2025-09-10 02:40:10 -0700

    Update Firebase configuration with new measurementId and appId
    - Updated measurementId from G-5Q7JKXX6Q2 to G-4F4QM0QENJ
    - Updated appId to 6b9f13cf1d3f9328a87ca2
    - Added Firebase Analytics initialization
    - Added site identifier 'csulbcpxlab' to configuration
    - Updated both index.html and firebase-config.js files

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,19 @@ const firebaseConfig = {
     projectId: "xin-s-hall-of-fame",
     storageBucket: "xin-s-hall-of-fame.firebasestorage.app",
     messagingSenderId: "692691719687",
-    appId: "1:692691719687:web:7bd1656dec1613e2a87ca2",
-    measurementId: "G-5Q7JKXX6Q2"
+    appId: "1:692691719687:web:6b9f13cf1d3f9328a87ca2",
+    measurementId: "G-4F4QM0QENJ"
+};
+
+// Site configuration
+const siteConfig = {
+    site: "csulbcpxlab"
 };
 
 // Export for use in other files
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = firebaseConfig;
+    module.exports = { firebaseConfig, siteConfig };
 } else {
     window.firebaseConfig = firebaseConfig;
+    window.siteConfig = siteConfig;
 }
